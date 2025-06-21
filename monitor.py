13c5ee91ba1f9b6be77e58006464db5b23804cc3
Jason Diller <jason@diller.ca>
2025-06-21 13:55:09 -0400

    Tidy up metrics names and remove unneeded function stubs

--- monitor.py
+++ monitor.py
@@ -5,23 +5,15 @@ from logconfig import configure_logging
 from datadog import initialize, statsd
 from pihole_client import Session, Api
 
-PREFIX = 'pihole.'
+PREFIX = 'pihole'
 
-def send_metrics(query_metrics:dict, prefix:str):
-    for sensor in query_metrics:
-        if type(query_metrics[sensor]) == dict:
+def send_metrics(metrics:dict, prefix:str):
+    for sensor in metrics:
+        if type(metrics[sensor]) == dict:
             prefix = f'{prefix}.{sensor}'
-            send_metrics(query_metrics[sensor], prefix)
+            send_metrics(metrics[sensor], prefix)
         else:
-            statsd.gauge(f'{prefix}.{sensor}', query_metrics[sensor])
-
-def send_status_metrics(status_metrics:dict):
-    for sensor in status_metrics:
-        statsd.gauge(f'{PREFIX}status.{sensor}', status_metrics[sensor])
-
-def send_replies_metrics(replies_metrics:dict):
-    for sensor in replies_metrics:
-        statsd.gauge(f'{PREFIX}replies.{sensor}', replies_metrics[sensor])
+            statsd.gauge(f'{prefix}.{sensor}', metrics[sensor])
 
 def main_loop(session:Session):
     while True:
