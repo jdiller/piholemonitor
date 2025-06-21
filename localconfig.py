7315fd098051abd45e4ce78d165d0ac03f244366
Jason Diller <jason@diller.ca>
2025-06-21 13:31:55 -0400

    Check for the existence of the config file.

--- localconfig.py
+++ localconfig.py
@@ -5,6 +5,8 @@ from configparser import ConfigParser
 def get_config() -> ConfigParser:
     config = ConfigParser()
     # repo defaults
+    if not os.path.exists(os.path.join(sys.path[0], 'config.ini')):
+        raise FileNotFoundError(f"config.ini not found in {sys.path[0]}")
     config.read(os.path.join(sys.path[0], 'config.ini'))
     return config
 
